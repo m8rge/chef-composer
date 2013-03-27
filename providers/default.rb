ca75be782e9ac325a8f4ac2858aa710578b75bc5
m8rge <to.merge@gmail.com>
2013-03-27 11:23:45 +0600

    fix recipe

--- providers/default.rb
+++ providers/default.rb
@@ -4,11 +4,8 @@ action :install do
 	directory new_resource.target_dir do
 		owner new_resource.owner
 		group new_resource.group
-		mode 00755
+		mode 0755
 		recursive true
-		action :create
-		
-		not_if "test -d #{new_resource.target_dir}"
 	end
 
 	remote_file "#{new_resource.target_dir}/composer.phar" do 
@@ -16,17 +13,14 @@ action :install do
 		owner new_resource.owner
 		group new_resource.group
 		mode 0755
-
-		not_if "test -f #{new_resource.target_dir}/composer.phar"
+		action :create_if_missing
 	end
 
 	link "#{new_resource.global_bin_dir}/composer" do
 		to "#{new_resource.target_dir}/composer.phar"
-
-		only_if {new_resource.global}
 	end
+	
 	new_resource.updated_by_last_action(true)
-
 end
 
 action :update do
